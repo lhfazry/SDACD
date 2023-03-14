6b47b3a49e1e20a2fa5821e86c909d34bcc5d22b
Fazry <lhuqita.fazry@gmail.com>
2023-03-14 14:33:06 +0700

    add setup

--- train.py
+++ train.py
@@ -70,6 +70,7 @@ if __name__ == '__main__':
 
     if opt.distributed:
         torch.cuda.set_device(args.local_rank)
+        opt.local_rank = args.local_rank
         dev = torch.device(f'cuda:{args.local_rank}')
         torch.distributed.init_process_group(
             backend="nccl", init_method="env://")
